61bcb1fe408e2dfbfb3791ef24cdfeded294ab0e
Espen Klem <espen.klem@gmail.com>
2021-04-30 22:10:54 +0200

    Better document processing demo More robust but not perfect. Good enough for demo purposes

--- demo/document-processing/document-processor-app.js
+++ demo/document-processing/document-processor-app.js
@@ -15,43 +15,66 @@ document.getElementById('headlinetext').onkeyup = function () {
 document.getElementById('bodytext').onkeyup = function () {
   daqProc()
 }
+// declaring some variables
+let emojiString
+let headlineStopped
+let bodyStopped
+let emojiArray
 
 // document and query processing
 const daqProc = function () {
   // Extract array of words and populate
-  let headlineString = document.getElementById('headlinetext').value
-  let bodyString = document.getElementById("bodytext").value
-  let emojiString = headlineString.concat(' ', bodyString)
-  let headlineArray = wnn.extract(headlineString, { regex: wnn.wordsNumbers, toLowercase: true })
-  let bodyArray = wnn.extract(bodyString, { regex: wnn.wordsNumbers, toLowercase: true })
-  let emojiArray = wnn.extract(emojiString, { regex: wnn.emojis })
+  const headlineString = document.getElementById('headlinetext').value
+  const bodyString = document.getElementById('bodytext').value
+  if (headlineString.length > 0 || bodyString.length > 0) {
+    emojiString = headlineString.concat(' ', bodyString)
+  }
+  const headlineArray = wnn.extract(headlineString, { regex: wnn.wordsNumbers, toLowercase: true })
+  const bodyArray = wnn.extract(bodyString, { regex: wnn.wordsNumbers, toLowercase: true })
+  emojiArray = wnn.extract(emojiString, { regex: wnn.emojis })
+
   populate(JSON.stringify(headlineArray, 2, ' '), 'headlineArrDiv')
   populate(JSON.stringify(bodyArray, 2, ' '), 'bodyArrDiv')
 
   // Prep for defining arrays of language to remove stopwords from.
   const languages = { af: sw.af, ar: sw.ar, bg: sw.bg, bn: sw.bn, br: sw.br, ca: sw.ca, cs: sw.cs, da: sw.da, de: sw.de, el: sw.el, en: sw.en, eo: sw.eo, es: sw.es, et: sw.et, eu: sw.eu, fa: sw.fa, fi: sw.fi, fr: sw.fr, ga: sw.ga, gl: sw.gl, ha: sw.ha, he: sw.he, hi: sw.hi, hr: sw.hr, hu: sw.hu, hy: sw.hy, id: sw.id, it: sw.it, ja: sw.ja, ko: sw.ko, la: sw.la, lgg: sw.lgg, lggo: sw.lggo, lv: sw.lv, mr: sw.mr, nl: sw.nl, no: sw.no, pl: sw.pl, pt: sw.pt, ptbr: sw.ptbr, pa: sw.pa, ro: sw.ro, ru: sw.ru, sk: sw.sk, sl: sw.sl, so: sw.so, st: sw.st, sv: sw.sv, sw: sw.sw, th: sw.th, tr: sw.tr, vi: sw.vi, yo: sw.yo, zh: sw.zh, zu: sw.zu }
-  let code = document.getElementById("languageSelected").value || en
+  const code = document.getElementById('languageSelected').value
   console.log(code)
+
   // Removing stopwords
-  let headlineStopped = sw.removeStopwords(headlineArray, languages[code])
-  populate(JSON.stringify(headlineStopped, 2, ' '), 'headlineStoppedDiv')
-  let bodyStopped = sw.removeStopwords(bodyArray, languages[code])
-  populate(JSON.stringify(bodyStopped, 2, ' '), 'bodyStoppedDiv')
+  if (headlineArray !== null) {
+    headlineStopped = sw.removeStopwords(headlineArray, languages[code])
+    populate(JSON.stringify(headlineStopped, 2, ' '), 'headlineStoppedDiv')
+  }
+
+  if (bodyArray !== null) {
+    bodyStopped = sw.removeStopwords(bodyArray, languages[code])
+    populate(JSON.stringify(bodyStopped, 2, ' '), 'bodyStoppedDiv')
+  }
 
   // Ngramming
-  let headlineNgrams = ngraminator(headlineStopped, [2,3,4])
-  populate(JSON.stringify(headlineNgrams, 2, ' '), 'headlineNgramifiedDiv')
-  let bodyNgrams = ngraminator(bodyStopped, [2,3,4])
-  populate(JSON.stringify(bodyNgrams, 2, ' '), 'bodyNgramifiedDiv')
+  if (headlineStopped !== null || headlineStopped.length > 1) {
+    const headlineNgrams = ngraminator(headlineStopped, [1, 2, 3, 4])
+    populate(JSON.stringify(headlineNgrams, 2, ' '), 'headlineNgramifiedDiv')
+  }
+
+  if (bodyStopped !== null || bodyStopped.length > 1) {
+    const bodyNgrams = ngraminator(bodyStopped, [1, 2, 3, 4])
+    populate(JSON.stringify(bodyNgrams, 2, ' '), 'bodyNgramifiedDiv')
+  }
 
   // Calculating keywords
-  let keywords = ehp.findKeywords(headlineStopped, bodyStopped, 5)
-  populate(JSON.stringify(keywords, 2, ' '), 'keywordsFoundDiv')
+  if (headlineStopped.length > 0 || bodyStopped.length > 0) {
+    const keywords = ehp.findKeywords(headlineStopped, bodyStopped, 5)
+    populate(JSON.stringify(keywords, 2, ' '), 'keywordsFoundDiv')
+  }
 
   // Emoji population
-  emojiArray = emojiArray.join('')
-  emojiArray = [...new Set(emojiArray)]
-  populate(JSON.stringify(emojiArray, 2, ' '), 'emojisFoundDiv')
+  if (emojiArray !== null && emojiArray.length > 0) {
+    emojiArray = emojiArray.join('')
+    emojiArray = [...new Set(emojiArray)]
+    populate(JSON.stringify(emojiArray, 2, ' '), 'emojisFoundDiv')
+  }
 }
 
 // Populating HTML elements with results
