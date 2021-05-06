1e94cebe7263ac76e4c692c90b23c24c0f0fefb3
Espen Klem <espen.klem@gmail.com>
2021-05-06 17:20:59 +0200

    Finishing up email extraction Both demos are okay now. New screenshots

--- demo/query-processing/query-processor-app.js
+++ demo/query-processing/query-processor-app.js
@@ -1,5 +1,5 @@
 // exposing the underlying libraries in a transparent way
-const { highlight, lvm } = dqp
+const { highlight, lvm, wnn } = dqp
 const index = ['a', 'adding', 'and', 'at', 'be', 'better', 'by', 'can', 'ensuring', 'even', 'get', 'gets', 'good', 'input', 'interesting', 'it', 'item', 'just', 'least', 'levenshtein', 'limit', 'long', 'longer', 'lots', 'make', 'match', 'matcher', 'maximum', 'maybe', 'more', 'need', 'nice', 'of', 'query', 'resembles', 'result', 'search', 'seems', 'so', 'some', 'text', 'that', 'the', 'this', 'to', 'we', 'will', 'with', 'words', 'work']
 
 // Populating div with only meaningful words
@@ -34,25 +34,31 @@ document.getElementById('fuzzy').onchange = function () {
 
 // calculate item highlighted
 const hitHighlight = function () {
-  let querytext = document.getElementById('querytext').value.split(' ')
-  const itemtext = 'some text that resembles a search result item with lots of nice words to match at least some of the query input and we can make it longer by adding even more interesting text so that maximum words limit gets interesting and it seems we need some more text to make the maximum words work better maybe this long text can be good and it will just get better and better the longer we make it ensuring lots of words that the query and levenshtein matcher can match'.split(' ')
+  const itemtext = 'some text that resembles a search result item with lots of nice words to match at least some of the query input and we can make it longer by adding even more interesting text so that maximum words limit gets interesting and it seems we need some more text to make the maximum words work better maybe this long text can be good and it will just get better and better the longer we make it ensuring lots of words that the query and levenshtein matcher can match'
   const itemmaxwords = document.getElementById('maxwords').value
   const fuzzyMatching = document.getElementById('fuzzy').checked
   let matchedQuery = ''
+  let querytext = ''
+  let hitHighlighted = itemtext
+  console.log('querytext: ' + document.getElementById('querytext').value)
+  if (document.getElementById('querytext').value !== '') {
+    querytext = document.getElementById('querytext').value
+    querytext = wnn.extract(querytext, { toLowercase: true })
 
-  // remove empty values in query array, to not get stupid matches from '' to 'a' for example.
-  querytext = querytext.filter(function (word) {
-    return word !== ''
-  })
-  if (fuzzyMatching) {
-    matchedQuery = lvm.levenMatch(querytext, index, { distance: 1 }).flat()
-  } else {
-    matchedQuery = querytext
+    // remove empty values in query array, to not get stupid matches from '' to 'a' for example.
+    querytext = querytext.filter(function (word) {
+      return word !== ''
+    })
+    if (fuzzyMatching) {
+      matchedQuery = lvm.levenMatch(querytext, index, { distance: 1 }).flat()
+    } else {
+      matchedQuery = querytext
+    }
+
+    console.log(matchedQuery)
+    hitHighlighted = highlight(matchedQuery, wnn.extract(itemtext, { toLowercase: true }), { itemMaxWords: itemmaxwords })
+    console.log('Hit(s) highlighted: ' + hitHighlighted)
   }
-
-  console.log(matchedQuery)
-  const hitHighlighted = highlight(matchedQuery, itemtext, { itemMaxWords: itemmaxwords })
-  console.log('Hit(s) highlighted: ' + hitHighlighted)
   populateItem(hitHighlighted)
 }
 
